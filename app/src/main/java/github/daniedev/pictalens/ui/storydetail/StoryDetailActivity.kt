8090e37528a42180a924bb5d2a0bd715ad1cd9a3
daniedev <a.danielpf21@gmail.com>
2022-10-05 12:39:31 +0530

    Progress bar for Story with auto switch to next Page

--- app/src/main/java/github/daniedev/pictalens/ui/storydetail/StoryDetailActivity.kt
+++ app/src/main/java/github/daniedev/pictalens/ui/storydetail/StoryDetailActivity.kt
@@ -1,8 +1,12 @@
 package github.daniedev.pictalens.ui.storydetail
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.ViewPropertyAnimator
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import github.daniedev.pictalens.databinding.ActivityStoryDetailBinding
 
 class StoryDetailActivity : AppCompatActivity() {
@@ -10,6 +14,8 @@ class StoryDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStoryDetailBinding
     private val storyDetailViewModel by viewModels<StoryDetailViewModel>()
     private val storyDetailPagerAdapter = StoryDetailPagerAdapter()
+    private lateinit var progressBarAnimator : ViewPropertyAnimator
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,24 @@ class StoryDetailActivity : AppCompatActivity() {
         binding.storyViewPager.adapter = storyDetailPagerAdapter
         val tagName = intent.getStringExtra("tag")
         tagName?.let { storyDetailViewModel.getGalleryBasedOnTag(it) }
+        binding.storyViewPager.registerOnPageChangeCallback(pageChangeCallback)
+        progressBarAnimator = binding.progressBar.animate()
+    }
+
+    private val nextPageRunnable = Runnable {
+        binding.storyViewPager.currentItem++
+    }
+
+    private val pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            progressBarAnimator.cancel()
+            binding.progressBar.scaleX = 0f
+            progressBarAnimator.scaleX(1f).setDuration(5000).setStartDelay(10).start()
+
+            handler.removeCallbacks(nextPageRunnable)
+            handler.postDelayed(nextPageRunnable, 5000)
+        }
     }
 
     override fun onResume() {
